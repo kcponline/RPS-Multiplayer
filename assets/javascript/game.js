bc001ce68d3ff1a6dd730955ab26e1054b2b057c
kcponline <kcponline@Admins-MacBook-Pro.local>
2016-08-14 16:01:20 -0700

    commit 04 - simultaneous store to session and firebase

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,49 +19,71 @@ $("#message2").html("jq link check");
 	// pseudo 01d: check all links - firebase
 $("#submit").on("click", function() {
 
-	// var player = $('#playerName').val().trim();
-	// 	console.log(player);
+	var player = $('#playerName').val().trim();
+		console.log(player);
+	var playerOrder = "player1";
 
 	// database.ref().set({
 	// 	players: player;   <-- syntax error: no need for ; when it's only 1 property.
 	// 	players: player
 	// });
 
-	// 
-	// seesionStorage.setItem("players", player);
-	// 	console.log(players);
+		// successful setItem to sessionStorage.
+	sessionStorage.setItem("playerName", player);
+		// console.log(players);
+	sessionStorage.setItem("playerOrder", playerOrder);
+
+	database.ref().set({
+		players: {
+			player1: {
+				choice: "",
+				losses: 0,
+				name: player,				
+				wins: 0,
+			}
+		}
+	});
+
 
 	// var playerName = $('#playerName').val().trim();
 	// 	console.log(player);
 
-	var player1 = {
-			choic: "",
-			losses: 0,
-			name: "kent",
-			wins: 0
-	};
+	// var players = {
+	// 	player1: player1
+	// }
 
-	var players = {
-		player1: player1
-	}
-
-	// var player2 = {
-	// 	choic: "",
-	// 	losses: 0,
-	// 	name: "omar",
-	// 	wins: 0
-	// };
-
-	database.ref().push(players);
-
-
-	// database.ref().push(players {
+	// database.ref().push(players {   <--- wrong! wild guess to push nesting object
 	// 	player2: player2
 	// });
 
 	// database.ref().push(1);
 
-	return false;
+		// successful pushing nesting objects
+	// var player1 = {
+	// 		choic: "",
+	// 		losses: 0,
+	// 		name: "kent",
+	// 		wins: 0
+	// };
+
+	// var player2 = {
+	// 		choic: "",
+	// 		losses: 0,
+	// 		name: "omar",
+	// 		wins: 0
+	// };
+
+	// var player = {
+	// 	players: {
+	// 		player1: player1,
+	// 		player2: player2
+	// 	}
+	// };
+
+	// database.ref().push(player);
+
+
+	// return false;
 
 });
 
