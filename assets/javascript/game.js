f8c6e5752df77077a3b488996b5c5de065118123
kcponline <kcponline@Admins-MacBook-Pro.local>
2016-08-14 12:58:03 -0700

    commit 02 - add more pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-  	// Initialize Firebase
+  	// pseudo 01a: check all links - Initialize Firebase
 var config = {
 	apiKey: "AIzaSyAyyfvqOVptnlcRlVNxonDHhw7ubLqkzgg",
 	authDomain: "prsmultiplayerproject.firebaseapp.com",
@@ -7,22 +7,30 @@ var config = {
 };
 firebase.initializeApp(config);
 
+
 var database = firebase.database();
 
-  	// check js link to be comment out
+  	// pseudo 01b: check all links - js - to be comment out
 document.getElementById("message1").innerHTML = "js link check";
-	// pseudo 02: check jq link
+	// pseudo 01c: check all links - jq - to be comment out
 $("#message2").html("jq link check");
 
+	// pseudo 01d: check all links - firebase
 $("#submit").on("click", function() {
 	var player = $('#playerName').val().trim();
 		console.log(player);
 
 	database.ref().push(player);
-		
+
 
 
 
 }
 
-	)
+	);
+
+	// pseudo 02: branch1 hard code skeleton (hcs) differentiate player instance 
+	// probably with seesionStorage.  Use if/else console.log to prove.
+
+
+	// Pseudo 03: branch2 hcs evaluate players inputs.
